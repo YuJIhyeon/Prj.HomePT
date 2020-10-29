050f0ca0945192d98275ddbca55df6adf4716c79
JungHo-jin <wjdghwlstmzn@naver.com>
2020-10-29 21:03:20 +0900

    init
    11111111111

---
+++ Assets/myScripts/HPbar.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HPbar : MonoBehaviour
+{
+    [SerializeField]
+    private Slider hpBar;
+
+    private float maxHp = 100;  // 최대 hp 상태
+    public static float curHp = 100; //현재 hp 상태
+
+    public static int MinusHP = 5;
+
+    float hpRatio = curHp;
+
+    void Start()
+    {
+        hpBar.value = (float)curHp / (float)maxHp;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))    //1차: spacebar keydown이 있을경우 hpBar가 10씩 떨어짐 >> 팔의 움직임을 조건으로 걸어 떨어지는 수치 변경
+        {
+            gradient.HP_FLAG = true;
+            if (curHp > 0)
+            {
+                curHp -= MinusHP;
+            }
+            else
+            {
+                curHp = 0;
+            }
+            hpRatio = (float)curHp / (float)maxHp;
+        }
+        handleHp();
+
+    }
+
+    private void handleHp()
+    {
+        hpBar.value = Mathf.Lerp(hpBar.value, hpRatio, Time.deltaTime * 10);
+    }
+
+}
---
+++ Assets/myScripts/TextManager.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TextManager : MonoBehaviour
+{
+
+    public GameObject PracticeText;
+    public Text setText;
+    public Text repText;
+
+    private int reps;
+    private int sets;
+
+    #region StopWatch
+    float timer;
+    float seconds;
+    float minutes;
+    float hours;
+
+    bool start;
+
+    [SerializeField] Text TimeText;
+    #endregion StopWatch
+
+
+
+    void Start()
+    {
+
+        reps = 0;
+        sets = 0;
+
+        start = false;
+        timer = 0;
+
+    }
+
+    
+    void Update()
+    {
+        StopWatch();
+    }
+
+
+    void StopWatch()
+    {
+        if (start)
+        {
+            timer += Time.deltaTime;
+            seconds = (int)(timer % 60);
+            minutes = (int)((timer / 60) % 60);
+            hours = (int)(timer / 3600);
+
+            TimeText.text = "Time: " + hours.ToString("00") + ":" + minutes.ToString("00") + ":" + seconds.ToString("00");
+        }   
+    }
+
+    public void StartTimer()
+    {
+        start = true;
+    }
+    public void StopTimer()
+    {
+        start = false;
+    }
+
+    public void ResetTimer()
+    {
+        start = false;
+        timer = 0;
+    }
+
+}
