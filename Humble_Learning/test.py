5ccef6b0c26a5290afcc0be026698bcf631f61de
YuJihyeon <starsand96@naver.com>
2020-11-02 20:28:46 +0900

    Make Data
    외부 정보로 데이터셋 제작
    전역변수로 원하는 만큼 데이터 제작 가능

---
+++ Humble_Learning/test.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+test=['a','b','c','d','e','f']
+
+#print(test)
+A = np.random.shuffle(test)
+print(type(A))
+
+print(A)
+if A=='a':
+    print(A)
+    print("a")
+
+
+if A=='b':
+    print(A)
+    print("b")
