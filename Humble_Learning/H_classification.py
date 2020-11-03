05ee0d17248d59e9d9e135cfae56e0355cde0144
YuJihyeon <starsand96@naver.com>
2020-11-04 00:20:24 +0900

    Logistic Learning
    사이킷런을 활용한 훈련, 검증 및 테스트

--- Humble_Learning/H_classification.py
+++ Humble_Learning/H_classification.py
@@ -1,9 +1,10 @@
-import sklearn as sk
 import numpy as np
+from sklearn.linear_model import LogisticRegression
 
+Data = dict()
 def get_input_data(filename):
     input_file = open(filename, 'r', encoding='UTF8')
-    Excercise=[]
+    Exercise=[]
 
     for line in input_file:
         tmp = []
@@ -15,13 +16,11 @@ def get_input_data(filename):
         for sig in line:
             tmp.append(float(sig))
 
-        Excercise.append(tmp.copy())
+        Exercise.append(tmp.copy())
 
-    return Excercise
+    return Exercise
 
 def TrainData() :
-
-    Data = dict()
 
     curl = get_input_data("덤벨컬_학습용.txt")
     Data["Curl"] = curl
@@ -29,18 +28,127 @@ def TrainData() :
     kickBack = get_input_data("덤벨킥백_학습용.txt")
     Data["kickBack"] = kickBack
 
-    #for d in Data.keys():
-        #print(f"{Data[d]}:{len(Data[d])}")
+    return Data
+
+def ValidateData() :
+
+    curl = get_input_data("덤벨컬_검증용.txt")
+    Data["Curl_V"] = curl
+
+    kickBack = get_input_data("덤벨킥백_검증용.txt")
+    Data["kickBack_V"] = kickBack
 
     return Data
 
-def GenerateDataForm(data):
-    pass
+def TestData() :
 
+    curl = get_input_data("덤벨컬_테스트용.txt")
+    Data["Curl_T"] = curl
+
+    kickBack = get_input_data("덤벨킥백_테스트용.txt")
+    Data["kickBack_T"] = kickBack
+
+    return Data
+
+def GenerateTrDataForm(data):
+    c = "Curl"
+    k = "kickBack"
+    npDataC = np.array(data[c]).reshape(len(data[c]), 2)
+    npDataK = np.array(data[k]).reshape(len(data[k]), 2)
+
+    inData=np.concatenate((npDataC, npDataK), axis=0)
+
+    #random.shuffle(inData)
+
+    y_label=[]
+    for i in range(0,len(data[c])):
+        x='Dumbbell curl'
+        y_label.append(x)
+
+
+    for i in range(0,len(data[k])):
+        x='Dumbbell kickBack'
+        y_label.append(x)
+
+    #random.shuffle(y_label)
+
+    print(np.unique(y_label, return_counts=True))
+
+    return inData, y_label
+
+def GenerateVDataForm(data):
+    c="Curl_V"
+    k="kickBack_V"
+    npDataC = np.array(data[c]).reshape(len(data[c]), 2)
+    npDataK = np.array(data[k]).reshape(len(data[k]), 2)
+
+    inData=np.concatenate((npDataC, npDataK),axis=0)
+
+    #random.shuffle(inData)
+
+    y_label=[]
+    for i in range(0,len(data[c])):
+        x='Dumbbell curl'
+        y_label.append(x)
+
+
+    for i in range(0,len(data[k])):
+        x='Dumbbell kickBack'
+        y_label.append(x)
+
+    #random.shuffle(y_label)
+
+    print(np.unique(y_label, return_counts=True))
+
+    return inData, y_label
+
+def GenerateTDataForm(data):
+    c = "Curl_T"
+    k = "kickBack_T"
+    npDataC = np.array(data[c]).reshape(len(data[c]), 2)
+    npDataK = np.array(data[k]).reshape(len(data[k]), 2)
+
+    inData=np.concatenate((npDataC, npDataK),axis=0)
+
+    #random.shuffle(inData)
+
+    y_label=[]
+    for i in range(0,len(data[c])):
+        x='Dumbbell curl'
+        y_label.append(x)
+
+
+    for i in range(0,len(data[k])):
+        x='Dumbbell kickBack'
+        y_label.append(x)
+
+    #random.shuffle(y_label)
+
+    print(np.unique(y_label, return_counts=True))
+
+    return inData, y_label
 
 def main():
     trainingD = TrainData()
-    #print(trainingD)
+    x_train, y_train=GenerateTrDataForm(trainingD)
 
+    ValidateD=ValidateData()
+    x_val,y_val=GenerateVDataForm(ValidateD)
+
+    TestD=TestData()
+    x_test, y_test=GenerateTDataForm(TestD)
+
+    softmax_reg = LogisticRegression(multi_class='multinomial', solver='lbfgs', max_iter=200, random_state=23)
+    softmax_reg.fit((x_train), (y_train))
+
+    softmax_reg.fit((x_val), (y_val))
+
+    Result = softmax_reg.predict_proba(x_test).round(2)
+    print(Result)
+
+    Score=softmax_reg.score((x_test), (y_test))
+    print(Score)
+    print()
+    print(Data)
 if __name__ == '__main__':
     main()
