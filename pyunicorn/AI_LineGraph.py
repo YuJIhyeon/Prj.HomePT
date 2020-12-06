1aa159ff59cc931801bdcb0a70a6faed3fd648c8
JungHo-jin <wjdghwlstmzn@naver.com>
2020-12-06 17:20:23 +0900

    3333
    코드 간결화

--- pyunicorn/AI_LineGraph.py
+++ pyunicorn/AI_LineGraph.py
@@ -10,187 +10,115 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Start biceps
-path_dir1 = 'C:/Users/Hojin/Desktop/Prj.HomePT/pyunicorn/data_2do'
-file_list1 = os.listdir(path_dir1)
-
 # Biceps original data
-cnt = 0
-for file_name in file_list1:
-    sp = list()
-    with open('./data_2do/'+file_name) as file:
-        row = []
-        for line in file:
-            if line == '\n':
-                break
-            sp = line.strip().split('\t')
-            row.append(list(map(float, sp[:3])))
+def original_data_input(file_list, data_type, output_folder):
+    cnt = 0
+    for file_name in file_list:
+        sp = list()
+        with open('./' + data_type + '/' + file_name) as file:
+            row = []
+            for line in file:
+                if line == '\n':
+                    break
+                sp = line.strip().split('\t')
+                row.append(list(map(float, sp[:3])))
 
-    sp.clear()
+        sp.clear()
 
-    t = []
-    rms = []
-    angle = []
-    for r in np.array(row):
-        t.append(r[0])
-        rms.append(r[1])
-        angle.append(r[2])
+        t = []
+        rms = []
+        angle = []
+        for r in np.array(row):
+            t.append(r[0])
+            rms.append(r[1])
+            angle.append(r[2])
 
-    count = len(rms)
+        count = len(rms)
 
-    tIndex = np.linspace(0, count / 10, count)
+        tIndex = np.linspace(0, count / 10, count)
 
-    plt.ylabel("Amplitude")
-    plt.xlabel("Time [s]")
-    plt.ylim(0, 180)
-    plt.plot(tIndex, rms, tIndex, angle, 'r-')
-    plt.savefig('./result_original_2do/' + str(cnt) + '.png')
-    cnt += 1
+        plt.ylabel("Amplitude")
+        plt.xlabel("Time [s]")
+        plt.ylim(0, 180)
+        plt.plot(tIndex, rms, tIndex, angle, 'r-')
+        plt.savefig('./' + output_folder + '/' + str(cnt) + '.png')
+        cnt += 1
 
-    plt.cla()
-    row.clear()
+        plt.cla()
+        row.clear()
+
 
 # Biceps augmentation
-cnt = 0
-for file_name in file_list1:
-    sp = list()
-    with open('./data_2do/'+file_name) as file:
-        row = []
-        for line in file:
-            if line == '\n':
-                break
-            sp = line.strip().split('\t')
-            row.append(list(map(float, sp[:3])))
+def augmentation_data(file_list, data_type, output_folder):
+    cnt = 0
+    for file_name in file_list:
+        sp = list()
+        with open('./'+data_type+'/' + file_name) as file:
+            row = []
+            for line in file:
+                if line == '\n':
+                    break
+                sp = line.strip().split('\t')
+                row.append(list(map(float, sp[:3])))
 
-    sp.clear()
+        sp.clear()
 
-    t = []
-    data1 = []
-    data2 = []
-    for r in np.array(row):
-        t.append(r[0])
-        data1.append(r[1])
-        data2.append(r[2])
+        t = []
+        data1 = []
+        data2 = []
+        for r in np.array(row):
+            t.append(r[0])
+            data1.append(r[1])
+            data2.append(r[2])
 
-    td = np.array([])
-    td = np.append(td, t, axis=0)
-    td = np.vstack([td, data1])
-    td = np.vstack([td, data2])
+        td = np.array([])
+        td = np.append(td, t, axis=0)
+        td = np.vstack([td, data1])
+        td = np.vstack([td, data2])
 
-    # AAFT
-    d = Surrogates(original_data=td, silence_level=2).AAFT_surrogates(td)
+        # AAFT
+        d = Surrogates(original_data=td, silence_level=2).AAFT_surrogates(td)
 
-    # IAAFT
-    #d = Surrogates(original_data=td, silence_level=2).refined_AAFT_surrogates(td,10000)
+        # IAAFT
+        # d = Surrogates(original_data=td, silence_level=2).refined_AAFT_surrogates(td,10000)
 
-    rms = d[1]
-    angle = d[2]
+        rms = d[1]
+        angle = d[2]
 
-    count = len(rms)
+        count = len(rms)
 
-    tIndex = np.linspace(0, count / 10, count)
+        tIndex = np.linspace(0, count / 10, count)
 
-    plt.ylabel("Amplitude")
-    plt.xlabel("Time [s]")
-    plt.ylim(0, 180)
-    plt.plot(tIndex, rms, tIndex, angle, 'r-')
-    plt.savefig('./result_augmentation_2do/' + str(cnt) + '.png')
-    cnt += 1
+        plt.ylabel("Amplitude")
+        plt.xlabel("Time [s]")
+        plt.ylim(0, 180)
+        plt.plot(tIndex, rms, tIndex, angle, 'r-')
+        plt.savefig('./'+output_folder+'/' + str(cnt) + '.png')
+        cnt += 1
 
-    plt.cla()
-    row.clear()
+        plt.cla()
+        row.clear()
 
-# Triceps
-path_dir2 = 'C:/Users/Hojin/Desktop/Prj.HomePT/pyunicorn/data_3do'
-file_list2 = os.listdir(path_dir2)
+def main():
+    # Biceps
+    path_dir1 = 'C:/Users/Hojin/Desktop/Prj.HomePT/pyunicorn/data_2do'
+    file_list1 = os.listdir(path_dir1)
 
-# Triceps original data
-cnt = 0
-for file_name in file_list2:
-    sp = list()
-    with open('./data_3do/'+file_name) as file:
-        row = []
-        for line in file:
-            if line == '\n':
-                break
-            sp = line.strip().split('\t')
-            row.append(list(map(float, sp[:3])))
+    original_data_input(file_list1, 'data_2do', 'result_original_2do')
+    augmentation_data(file_list1, 'data_2do', 'result_augmentation_2do')
 
-    sp.clear()
+    # Triceps
+    path_dir2 = 'C:/Users/Hojin/Desktop/Prj.HomePT/pyunicorn/data_3do'
+    file_list2 = os.listdir(path_dir2)
 
-    t = []
-    rms = []
-    angle = []
-    for r in np.array(row):
-        t.append(r[0])
-        rms.append(r[1])
-        angle.append(r[2])
+    original_data_input(file_list2, 'data_3do', 'result_original_3do')
+    augmentation_data(file_list2, 'data_3do', 'result_augmentation_3do')
 
-    count = len(rms)
+    print("Finished")
 
-    tIndex = np.linspace(0, count / 10, count)
+main()
 
-    plt.ylabel("Amplitude")
-    plt.xlabel("Time [s]")
-    plt.ylim(0, 180)
-    plt.plot(tIndex, rms, tIndex, angle, 'r-')
-    plt.savefig('./result_original_3do/' + str(cnt) + '.png')
-    cnt += 1
 
-    plt.cla()
-    row.clear()
-
-# Triceps data augmentation
-cnt = 0
-for file_name in file_list2:
-    sp = list()
-    with open('./data_3do/'+file_name) as file:
-        row = []
-        for line in file:
-            if line == '\n':
-                break
-            sp = line.strip().split('\t')
-            row.append(list(map(float, sp[:3])))
-
-    sp.clear()
-
-    t = []
-    data1 = []
-    data2 = []
-    for r in np.array(row):
-        t.append(r[0])
-        data1.append(r[1])
-        data2.append(r[2])
-
-    td = np.array([])
-    td = np.append(td, t, axis=0)
-    td = np.vstack([td, data1])
-    td = np.vstack([td, data2])
-
-    # AAFT
-    d = Surrogates(original_data=td, silence_level=2).AAFT_surrogates(td)
-
-    # IAAFT
-    # d = Surrogates(original_data=td, silence_level=2).refined_AAFT_surrogates(td,10000)
-
-    rms = d[1]
-    angle = d[2]
-
-    count = len(rms)
-
-    tIndex = np.linspace(0, count / 10, count)
-
-    plt.ylabel("Amplitude")
-    plt.xlabel("Time [s]")
-    plt.ylim(0, 180)
-    plt.plot(tIndex, rms, tIndex, angle, 'r-')
-    plt.savefig('./result_augmentation_3do/' + str(cnt) + '.png')
-    cnt += 1
-
-    plt.cla()
-    row.clear()
-
-print("Finished")
 # In[ ]:
 
 
