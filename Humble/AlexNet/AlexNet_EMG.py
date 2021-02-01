741f7c202f79f605693b199df3f49d89baf82f7c
JungHo-jin <56426289+JungHo-jin@users.noreply.github.com>
2021-02-01 22:33:06 +0900

    20210201-2

--- Humble/AlexNet/AlexNet_EMG.py
+++ Humble/AlexNet/AlexNet_EMG.py
@@ -2,19 +2,19 @@ import tensorflow as tf
 from tensorflow import keras
 
 import numpy as np
-import matplotlib.pyplot as plt
 
 config = tf.compat.v1.ConfigProto()
 config.gpu_options.per_process_gpu_memory_fraction = 0.4
 session = tf.compat.v1.InteractiveSession(config=config)
 
-# 파일 불러오기
 import os, re, glob
 import cv2
 
 image_size = 227
 
-groups_folder_path = '../Images/After_AAFT/'
+#groups_folder_path = '../Images/After_AAFT/'
+#groups_folder_path = '../Images/After_image/'
+groups_folder_path = '../Images/After_resultFigs/'
 
 X_s = []
 
@@ -22,14 +22,15 @@ y = []
 
 for label in ['Bi', 'Tri']:
 
-    for feature in ['striping_color']:
+    for feature in ['striping_bw']:
 
         for top, dir, f in os.walk(groups_folder_path + label + "/" + feature + "/"):
+            print(f)
             for filename in f:
+                # print(groups_folder_path + label + "/" + feature + "/" + filename)
                 img = cv2.imread(groups_folder_path + label + "/" + feature + "/" + filename)
                 img = cv2.resize(img, None, fx=image_size / img.shape[0], fy=image_size / img.shape[1])
 
-                #if feature == 'striping_bw':
                 X_s.append(img / 256)
 
                 if label == 'Bi':
@@ -37,65 +38,7 @@ for label in ['Bi', 'Tri']:
                 else:
                     y.append(3)
 
-print(X_s[0].shape)
+print(X_s[0])
 
-'''
-X = []
-
-for s in X_s:
-    img = []
-    for i in range(len(s)):
-        #row = np.concatenate((s[i]), axis=1)
-        img.append(row)
-    X.append(img)
-'''
 X = np.array(X_s)
 y = np.array(y)
-
-# train, test split
-from sklearn.model_selection import train_test_split
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
-
-# validation split
-
-X_train, X_validation, y_train, y_validation = train_test_split(X_train, y_train, test_size=0.1, random_state=10, stratify=y_train)
-
-AlexNet = keras.models.Sequential([
-        keras.layers.Conv2D(256, (6, 6), activation='relu', input_shape=X_train[0].shape),
-        keras.layers.MaxPooling2D( (2, 2) ),
-        keras.layers.BatchNormalization(),
-
-
-        keras.layers.Conv2D(384, (3, 3), padding='same', activation='relu'),
-        keras.layers.MaxPooling2D( (2, 2) ),
-        keras.layers.BatchNormalization(),
-
-        keras.layers.Conv2D(384, (3, 3), padding='same', activation='relu'),
-        keras.layers.MaxPooling2D( (2, 2) ),
-        keras.layers.BatchNormalization(),
-
-        keras.layers.Conv2D(256, (3, 3), padding='same', activation='relu'),
-        keras.layers.MaxPooling2D( (2, 2) ),
-        keras.layers.BatchNormalization(),
-
-        keras.layers.Flatten(),
-        keras.layers.Dense(1024, activation='relu'),
-        keras.layers.Dense(1024, activation='relu'),
-
-        keras.layers.Dense(len(y_train), activation='softmax')
-])
-
-AlexNet.summary()
-
-AlexNet.compile(loss=keras.losses.sparse_categorical_crossentropy,
-              optimizer=keras.optimizers.SGD(learning_rate=0.01),
-              metrics=[keras.metrics.sparse_categorical_accuracy])
-
-AlexNet_history = AlexNet.fit(X_train, y_train, epochs=100, batch_size=16,
-                    validation_data=(X_validation, y_validation))
-
-AlexNet_history = AlexNet.fit(X_train, y_train, epochs=10, batch_size=16,
-                    validation_data=(X_test, y_test))
-
-AlexNet_history.history['accuracy']
